6ce8006fe514453bb29fd0784aa38557eb1ebed4
MacVinny <macvinny@protonmail.com>
2021-08-15 18:48:49 -0300

    Institution service

---
+++ src/app/institution.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Institution } from './institution';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class InstitutionService {
+    private apiServerUrl = environment.apiBaseUrl;
+
+    constructor(private http: HttpClient) {}
+
+    public getInstitution(): Observable<Institution[]> {
+        return this.http.get<Institution[]>(`${this.apiServerUrl}/institution/all`);
+    }
+
+    public addInstitution(institution: Institution): Observable<Institution> {
+        return this.http.post<Institution>(`${this.apiServerUrl}/institution/add`, institution);
+    }
+    
+    public updateInstitution(institution: Institution): Observable<Institution> {
+        return this.http.put<Institution>(`${this.apiServerUrl}/institution/update`, institution);
+    }
+    
+    public deleteInstitution(institutionId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/institution/delete/${institutionId}`);
+    }
+}
---
+++ src/app/institution.ts
@@ -0,0 +1,9 @@
+export interface Institution {
+    id: number;
+    name: string;
+    phone: string;
+    email: string;
+    address: string;
+    imageUrl: string;
+    institutionCode: string;
+}
